8f9548c10714ab5cbbc0ce8b345dd196912d6a07
gillancodes <gillancodes@laposte.net>
2024-12-13 20:21:56 +0100

    feat: added telescope mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "f", "<cmd>Telescope find_files<cr>")
+
 map("n", "<C-t>", function()
   require("nvchad.themes").open()
 end, {})
