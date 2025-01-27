b7c2ddefee0a5d0d03e09eaa2effa1e919586742
gillancodes <gillancodes@laposte.net>
2025-01-28 00:18:28 +0100

    feat: added lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "ts_ls", "phpactor" }
+local servers = { "html", "cssls", "clangd", "ts_ls", "phpactor", "tailwindcss", "intelephense"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
