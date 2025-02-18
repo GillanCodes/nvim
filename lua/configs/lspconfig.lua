faf7573a89cd397ea4c29a75824d1dc7c1a93c00
gillancodes <gillancodes@laposte.net>
2025-02-18 12:43:35 +0100

    feat: added twig support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -17,7 +17,7 @@ for _, lsp in ipairs(servers) do
 end
 
 lspconfig.emmet_language_server.setup({
-  filetypes = { "php", "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+  filetypes = { "twig", "php", "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
   -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
   -- **Note:** only the options listed in the table are supported.
   init_options = {
