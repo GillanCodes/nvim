57061ee3f9697ccd4c54d011d0861b03b43c0260
gillancodes <gillancodes@laposte.net>
2025-04-04 19:53:05 +0200

    fix: 1row up

--- lua/plugins/pets.lua
+++ lua/plugins/pets.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
     config = function()
       require("pets").setup({
-        row = 6,                 -- the row (height) to display the pet at (higher row means the pet is lower on the screen), must be 1<=row<=10
+        row = 5,                 -- the row (height) to display the pet at (higher row means the pet is lower on the screen), must be 1<=row<=10
         col = 0,                 -- the column to display the pet at (set to high number to have it stay still on the right side)
         speed_multiplier = 1,    -- you can make your pet move faster/slower. If slower the animation will have lower fps.
         random = true,           -- whether to use a random pet for the PetNew command, overrides default_pet and default_style
