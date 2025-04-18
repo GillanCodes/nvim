b14da07387fd50844a1b104a163bcf950be0ac91
gillancodes <gillancodes@laposte.net>
2025-04-18 12:07:49 +0200

    feat: added html completion to jsx/tsx

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls" },
+				ensure_installed = { "lua_ls", "ts_ls", "emmet_ls" },
 			})
 		end,
 	},
@@ -27,12 +27,34 @@ return {
 			}
 
 			capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
-			-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			-- capabilities = require('cmp_nvim_lsp').default_capabilities()
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
+			})
+
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"html",
+					"css",
+					"scss",
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"jsx",
+					"tsx",
+				},
+				init_options = {
+					jsx = {
+						options = {
+							["jsx.enabled"] = true,
+						},
+					},
+				},
 			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
