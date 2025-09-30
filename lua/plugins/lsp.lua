8597ab4e1981d6d24207e00edaefd4e058d4d43d
gillancodes <gillancodes@laposte.net>
2025-09-30 12:21:08 +0200

    feat: added php

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,69 @@
+-- return {
+-- 	{
+-- 		"williamboman/mason.nvim",
+-- 		config = function()
+-- 			require("mason").setup()
+-- 		end,
+-- 	},
+-- 	{
+-- 		"williamboman/mason-lspconfig.nvim",
+-- 		config = function()
+-- 			require("mason-lspconfig").setup({
+-- 				ensure_installed = { "lua_ls", "ts_ls", "emmet_ls", "phpactor" },
+-- 			})
+-- 		end,
+-- 	},
+-- 	{
+-- 		"neovim/nvim-lspconfig",
+-- 		config = function()
+-- 			local lspconfig = require("lspconfig")
+-- 			local capabilities = {
+-- 				textDocument = {
+-- 					foldingRange = {
+-- 						dynamicRegistration = false,
+-- 						lineFoldingOnly = true,
+-- 					},
+-- 				},
+-- 			}
+--
+-- 			capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+-- 			-- capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- 			lspconfig.lua_ls.setup({
+-- 				capabilities = capabilities,
+-- 			})
+-- 			lspconfig.ts_ls.setup({
+-- 				capabilities = capabilities,
+-- 			})
+--
+-- 			lspconfig.emmet_ls.setup({
+-- 				capabilities = capabilities,
+-- 				filetypes = {
+-- 					"html",
+-- 					"css",
+-- 					"scss",
+-- 					"javascript",
+-- 					"javascriptreact",
+-- 					"typescript",
+-- 					"typescriptreact",
+-- 					"jsx",
+-- 					"tsx",
+-- 				},
+-- 				init_options = {
+-- 					jsx = {
+-- 						options = {
+-- 							["jsx.enabled"] = true,
+-- 						},
+-- 					},
+-- 				},
+-- 			})
+--
+-- 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+-- 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+-- 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+-- 		end,
+-- 	},
+-- }
+
 return {
 	{
 		"williamboman/mason.nvim",
@@ -9,34 +75,54 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "emmet_ls" },
+				ensure_installed = { "lua_ls", "ts_ls", "emmet_ls", "phpactor" },
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = {
-				textDocument = {
-					foldingRange = {
-						dynamicRegistration = false,
-						lineFoldingOnly = true,
-					},
-				},
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+			-- Add folding capabilities
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
 			}
 
-			capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
-			-- capabilities = require('cmp_nvim_lsp').default_capabilities()
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
+			-- Merge with blink.cmp capabilities if available
+			local has_blink, blink = pcall(require, "blink.cmp")
+			if has_blink then
+				capabilities = blink.get_lsp_capabilities(capabilities)
+			end
+
+			-- Lua LSP
+			vim.lsp.config("lua_ls", {
+				cmd = { "lua-language-server" },
+				root_markers = {
+					".luarc.json",
+					".luarc.jsonc",
+					".luacheckrc",
+					".stylua.toml",
+					"stylua.toml",
+					"selene.toml",
+					"selene.yml",
+					".git",
+				},
 				capabilities = capabilities,
 			})
 
-			lspconfig.emmet_ls.setup({
+			-- TypeScript LSP
+			vim.lsp.config("ts_ls", {
+				cmd = { "typescript-language-server", "--stdio" },
+				root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
 				capabilities = capabilities,
+			})
+
+			-- Emmet LSP
+			vim.lsp.config("emmet_ls", {
+				cmd = { "emmet-ls", "--stdio" },
+				root_markers = { ".git", "package.json" },
 				filetypes = {
 					"html",
 					"css",
@@ -45,21 +131,19 @@ return {
 					"javascriptreact",
 					"typescript",
 					"typescriptreact",
-					"jsx",
-					"tsx",
 				},
-				init_options = {
-					jsx = {
-						options = {
-							["jsx.enabled"] = true,
-						},
-					},
-				},
+				capabilities = capabilities,
 			})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			-- Phpactor LSP
+			vim.lsp.config("phpactor", {
+				cmd = { "phpactor", "language-server" },
+				root_markers = { "composer.json", ".git" },
+				capabilities = capabilities,
+			})
+
+			-- Enable LSP servers
+			vim.lsp.enable({ "lua_ls", "ts_ls", "emmet_ls", "phpactor" })
 		end,
 	},
 }
